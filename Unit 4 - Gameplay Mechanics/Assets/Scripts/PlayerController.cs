1973c7041be243c53eabdae768829bd7c23816d2
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-02 17:52:20 +0200

    Powerup

---
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    public float speed;
+
+    [Header("Powerup")]
+    public float powerupStrength = 15f;
+    public float powerupTime = 5f;
+
+    public GameObject powerupIndicator;
+
+    private bool hasPowerup;
+
+    [Header("Unity stuff")]
+    public Transform focalPoint;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        instance = this;
+    }
+    private void Update()
+    {
+        MovePlayer();
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void MovePlayer()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(focalPoint.forward * speed * verticalInput);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdown());
+        }
+    }
+    private IEnumerator PowerupCountdown()
+    {
+        yield return new WaitForSeconds(powerupTime);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+}
