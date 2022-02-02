109472174100a42a21699b6a06c14bed37cec980
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-02 16:05:59 +0200

    Base player controller

---
+++ Unit 4 - Gameplay Mechanics/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public Transform focalPoint;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(focalPoint.forward * speed * verticalInput);
+    }
+}
