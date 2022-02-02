472f46c8440d61b16e4a8e42bba36dd9805a6eff
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-02 16:28:41 +0200

    Enemy

---
+++ Unit 4 - Gameplay Mechanics/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+
+    private Rigidbody rb;
+    private Vector3 lookDirection;
+    private GameObject player;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        lookDirection = (player.transform.position - transform.position).normalized;
+
+        rb.AddForce(lookDirection * speed);
+    }
+}
