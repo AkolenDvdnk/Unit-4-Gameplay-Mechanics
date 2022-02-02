a045939459816f7abc33c399230647039c3c9f3c
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-02 15:55:10 +0200

    Camera horizontal input

---
+++ Unit 4 - Gameplay Mechanics/Assets/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, -horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
